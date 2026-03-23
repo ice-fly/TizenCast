bd94774ce8ea61a627f41ae9f1c6cc15a321ef25
ice-fly <ice_fly.alex@yahoo.com>
2026-03-23 12:22:53 -0400

    Update main.js
    Opcodes and transport cleanup

--- app/main.js
+++ app/main.js
@@ -1,57 +1,63 @@
-/// Bridges FCast SDK to Tizen Video Player
 (function() {
     'use strict';
+    
     const videoElement = document.getElementById('player');
     let receiver = null;
 
     function init() { 
-        /// Init Receiver Port 46899 is the standard FCast port
-        console.log("TizenCast: Initializing FCast Receiver...");
+        console.log("TizenCast: Initializing...");
+        
         receiver = new FCastReceiver();
-        receiver.on('connect', (client) => { //On client connected
+
+        receiver.on('connect', (client) => { //On connection established
             console.log("TizenCast: Connected to " + client.name);
-            window.dispatchEvent(new CustomEvent('fcast-receive-start'));// Notify UI
+            window.dispatchEvent(new CustomEvent('fcast-receive-start'));
         });
-        receiver.on('stream', (content) => { //On stream started
-            console.log("TizenCast: Stream received", content);
-            if (content && content.url) { // Pass the data to cast.html UI logic
+
+        receiver.on('stream', (content) => { //On stream received
+            if (content && content.url) {
                 window.dispatchEvent(new CustomEvent('fcast-stream-start', { 
                     detail: { url: content.url } 
                 }));
             }
         });
-        /// Playback Controls (Commands from Android)
+        //Transport Controls
         receiver.on('play', () => videoElement.play());
         receiver.on('pause', () => videoElement.pause());
         receiver.on('seek', (time) => { videoElement.currentTime = time; });
         receiver.on('volume', (vol) => { videoElement.volume = vol; });
-        receiver.on('stop', () => { //On stop / disconnect
-            console.log("TizenCast: Stopping stream.");
+        
+        receiver.on('stop', () => {
             window.dispatchEvent(new CustomEvent('fcast-stream-stop'));
         });
 
-        receiver.start().then(() => { //On start the Service
+        setInterval(() => { //OpCode 6 sync
+            if (receiver && receiver.isStreaming && receiver.socket) {
+                receiver.sendPlaybackUpdate(
+                    videoElement.currentTime, 
+                    videoElement.duration, 
+                    videoElement.paused
+                );
+            }
+        }, 2000);
+
+        receiver.start().then(() => {
             updateIP();
-        }).catch(err => {
-            console.error("TizenCast: Start failed", err);
         });
     }
 
-    /// Helper to show IP on screen 
-    function updateIP() {
+    function updateIP() { //Display IP
         const ipDisplay = document.getElementById('ip-info');
         if (!ipDisplay) return;
         try {
-            // Tizen-specific way to get local IP
-            var network = tizen.systeminfo.getPropertyValue("NETWORK", function(info) {
+            tizen.systeminfo.getPropertyValue("NETWORK", function(info) {
                 ipDisplay.innerText = "IP: " + info.ipAddress + " | Port: 46899";
             });
         } catch (e) {
-            ipDisplay.innerText = "Check network connection";
+            ipDisplay.innerText = "Check Network Settings";
         }
     }
-    // Standard Tizen startup
+
     window.onload = init;
-    // Export receiver to window so cast.html can call stop()
     window.fcast = receiver;
 })();
